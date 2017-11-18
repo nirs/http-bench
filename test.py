283f2562ad352918a624fb108fd68b2ce2867dcc
Nir Soffer <nsoffer@redhat.com>
2017-11-18 02:16:45 +0200

    Add parallel tests
    Fix #4.

--- test.py
+++ test.py
@@ -1,10 +1,11 @@
 import os
 import subprocess
 import threading
+import time
 
 import pytest
 
-SIZE_MB = os.environ.get("UPLOAD_SIZE_MB", "1024")
+SIZE_MB = int(os.environ.get("UPLOAD_SIZE_MB", "1024"))
 
 
 @pytest.fixture(scope="session")
@@ -24,19 +25,58 @@ def server():
 @pytest.mark.parametrize("variant", ["httplib", "requests"])
 @pytest.mark.parametrize("blocksize",
     [8, 32, 64, 128, 256, 512, 1024, 2048, 4096])
-def test_single_upload_python(server, variant, blocksize):
+def test_python(server, variant, blocksize):
     print(upload(variant, blocksize))
 
 
-def test_single_upload_go(server):
+def test_go(server):
     # Go uses hardcoded value, not way to change the blocksize.
     print(upload("go", 4))
 
 
-def upload(variant, blocksize_kb):
+@pytest.mark.parametrize("variant", ["httplib", "requests"])
+@pytest.mark.parametrize("blocksize", [32, 64, 128, 256, 512])
+def test_python_parallel(server, variant, blocksize):
+    print(upload_parallel(variant, blocksize))
+
+
+def test_go_parallel(server):
+    print(upload_parallel("go", 4))
+
+
+def upload_parallel(variant, blocksize_kb):
+    cpu_count = os.sysconf("SC_NPROCESSORS_ONLN")
+    size_per_worker = SIZE_MB // cpu_count
+    if size_per_worker == 0:
+        size_per_worker = 1
+
+    def run():
+        upload(variant, blocksize_kb, size_mb=size_per_worker)
+
+    start = time.time()
+
+    threads = []
+    try:
+        for i in range(cpu_count):
+            t = threading.Thread(target=run, name="upload/%d" % i)
+            t.daemon = True
+            t.start()
+            threads.append(t)
+
+    finally:
+        for t in threads:
+            t.join()
+
+    elapsed = time.time() - start
+
+    return "Uploaded %.2f MiB in %.2f seconds (%.2f MiB/s)" % (
+        SIZE_MB, elapsed, SIZE_MB / elapsed)
+
+
+def upload(variant, blocksize_kb, size_mb=SIZE_MB):
     cmd = [
         "./upload-%s" % variant,
-        "--size-mb", SIZE_MB,
+        "--size-mb", str(size_mb),
         "--blocksize-kb", str(blocksize_kb),
         "/dev/zero", "https://localhost:8000/"
     ]
